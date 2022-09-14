2ba788296bcb52519734d492ff4e3c29ddc0b119
Gustavo Silva <gustavo.silva@peraton.com>
2022-09-14 16:39:57 -0400

    added function to add a header value and combined a few files

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreData
 
 public final class APIClient: AsyncNetworkAPI, URLRequestBuilder {
     
@@ -23,4 +24,54 @@ public final class APIClient: AsyncNetworkAPI, URLRequestBuilder {
     public convenience init() {
         self.init(configuration: .default)
     }
+}
+
+extension APIClient {
+    
+    public func createDecoderWithDate() -> JSONDecoder {
+        // Create the decoder
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ss"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+    
+    public func createDecoder() -> JSONDecoder {
+        // Create the decoder
+        let decoder = JSONDecoder()
+        return decoder
+    }
+    
+    public func createDecoderWithDate(managedObjectContext: NSManagedObjectContext) -> JSONDecoder {
+        // Create the decoder
+        let decoder = JSONDecoder()
+        decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = managedObjectContext
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ss"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+    
+    public func createDecoder(managedObjectContext: NSManagedObjectContext) -> JSONDecoder {
+        // Create the decoder
+        let decoder = JSONDecoder()
+        decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = managedObjectContext
+        return decoder
+    }
+    
+    public func setUpSecurityHeaders(for jsonWebToken: String, request: inout URLRequest) {
+        // Added for OAuth
+        if (!jsonWebToken.isEmpty) {
+            request.addValue("Bearer \(jsonWebToken)" , forHTTPHeaderField: "Authorization")
+        }
+    }
+    
+    public func setUpHeader(for headerField: String, headerValue: String, request: inout URLRequest) {
+            request.addValue(headerValue , forHTTPHeaderField: headerField)
+    }
+}
+
+extension CodingUserInfoKey {
+    public static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")
 }
